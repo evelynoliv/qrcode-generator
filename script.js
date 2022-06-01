a74f9cd266c9043b65c24e85de7ab8c472a42ea6
Evelyn O <evelynolives@gmail.com>
2022-06-01 16:11:54 -0300

    updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function QRCodeGenerator(){
     var userInput = document.querySelector('textarea').value;
     var chartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=H&chl=';
-    var qrCodeContent = chartAPI + userInput;
+    var qrCodeContent = chartAPI + encodeURIComponent(userInput);
     document.querySelector('#QRCodeImage').src = qrCodeContent;
+    alert(qrCodeContent);
 }
